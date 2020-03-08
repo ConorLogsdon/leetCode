40b3f3c3d29d0714e57307d921a1c1d42ffd671e
ConorLogsdon <logsdonconor@gmail.com>
2020-03-08 16:18:18 -0700

    Update 13. Roman to Integer.py

--- LeetCode/13. Roman to Integer.py
+++ LeetCode/13. Roman to Integer.py
@@ -1,7 +1,7 @@
 class Solution:
     def romanToInt(self, s):
         sysms = {'I':1, 'V':5, 'X':10,'L':50,'C':100,'D':500,'M':1000}
-        result, temp = 0, 0
+        result, temp
         
         for letters in reversed(s):
             number = sysms[letters]
